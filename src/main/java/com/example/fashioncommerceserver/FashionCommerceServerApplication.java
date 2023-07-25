89ba6a2f9f0f20fb9b019bd370c0e744049d5fef
shindawull <juoiy05@naver.com>
2023-07-25 19:01:59 +0900

    Initial commit

---
+++ src/main/java/com/example/fashioncommerceserver/FashionCommerceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fashioncommerceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionCommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionCommerceServerApplication.class, args);
+	}
+
+}
