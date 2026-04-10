419096776187a97d242247c2b1336eb494c90b71
shindawull <juoiy05@naver.com>
2026-04-10 17:34:40 +0900

    refactor : (feature/oauth) Annotation 제거 - @JsonProperty 제거 (불필요해짐)

--- src/main/java/com/ccommit/fashionserver/dto/UserDto.java
+++ src/main/java/com/ccommit/fashionserver/dto/UserDto.java
@@ -2,16 +2,17 @@ package com.ccommit.fashionserver.dto;
 
 import com.ccommit.fashionserver.service.PhoneNumCheck;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
 import java.sql.Date;
 
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@ToString
 public class UserDto {
     private int id;                //번호
 
@@ -23,25 +24,25 @@ public class UserDto {
 
     @NotBlank
     @PhoneNumCheck
-    @JsonProperty("phone_number")
     private String phoneNumber;    //휴대폰번호
 
-    @JsonProperty("create_date")
     private Date createDate;       //생성날짜
 
-    @JsonProperty("update_date")
     private Date updateDate;       //수정날짜
 
-    @JsonProperty("is_join")
     private boolean isJoin;        //가입상태
 
-    @JsonProperty("is_withdraw")
     private boolean isWithdraw;    //탈퇴상태
 
     @NotBlank
-    @JsonProperty("user_id")
     private String userId;         //아이디
 
-    @JsonProperty("user_type")
     private UserType userType;      //관리자여부
+
+    // OAuth 추가 필드
+    private String oauthProvider;   // 소셜 로그인 제공자(google, naver)
+
+    private String oauthId;         // 소셜 로그인 고유ID
+
+    private int isProfileComplete;  // 추가 정보 입력 완료 여부
 }
